1ecf829591d318ac24316ca51ca4017dcbbdfd1c
jamesdarylbertiz@gmail.com <75722677+jdbbdj@users.noreply.github.com>
2022-02-18 14:03:58 +0800

    ReactJava adding snackbar success upon right credentials

--- src/components/LoginComponents/index.jsx
+++ src/components/LoginComponents/index.jsx
@@ -1,16 +1,33 @@
 import React, { useState } from "react";
-import { Container, TextField, Button } from "@material-ui/core";
+import { Container, TextField, Button, Snackbar } from "@material-ui/core";
+import Alert from "@material-ui/lab/Alert";
+import AlertTitle from "@material-ui/lab/AlertTitle";
 import "./LoginComponents.css";
 import INITIAL_VALUES from "./initialValues";
 
 const LoginComponents = () => {
   const [initialValues, setInitialValues] = useState(INITIAL_VALUES);
+  const [snackBarStatus, setSnackBarStatus] = useState(false);
+  const [hasLogin, setHasLogin] = useState(false);
 
   const handleChange = (e) => {
     // create the new state and set it
     setInitialValues((prev) => ({ ...prev, [e.target.name]: e.target.value }));
 
     e.preventDefault();
+  };
+
+  const loginClick = () => {
+    if (
+      initialValues.username === "james" &&
+      initialValues.password === "test"
+    ) {
+      setSnackBarStatus(true);
+      setHasLogin(true);
+    } else {
+      setSnackBarStatus(true);
+      setHasLogin(false);
+    }
   };
 
   return (
@@ -35,9 +52,21 @@ const LoginComponents = () => {
           />
         </div>
         <div>
-          <Button>Login</Button>
+          <Button onClick={loginClick}>Login</Button>
         </div>
       </form>
+
+      <Snackbar severity="error" open={snackBarStatus}>
+        {hasLogin ? (
+          <Alert severity="success" sx={{ width: "100%" }}>
+            Success
+          </Alert>
+        ) : (
+          <Alert severity="error" sx={{ width: "100%" }}>
+            Error
+          </Alert>
+        )}
+      </Snackbar>
     </Container>
   );
 };
