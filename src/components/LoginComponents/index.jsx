f27de7e8197fd2fb117f78de9fc69e1093ae36df
jamesdarylbertiz@gmail.com <75722677+jdbbdj@users.noreply.github.com>
2022-02-18 13:21:24 +0800

    add initial values and common handleChange

--- src/components/LoginComponents/index.jsx
+++ src/components/LoginComponents/index.jsx
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, TextField, Button } from "@material-ui/core";
 import "./LoginComponents.css";
+import INITIAL_VALUES from "./initialValues";
 
 const LoginComponents = () => {
+  const [initialValues, setInitialValues] = useState(INITIAL_VALUES);
+
+  const handleChange = (e) => {
+    // create the new state and set it
+    setInitialValues((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+
+    e.preventDefault();
+  };
+
   return (
     <Container className="loginCont">
       <form className="loginForm">
         <div>
           <label>Username</label>
-          <TextField type="text" name="username" />
+          <TextField
+            type="text"
+            name="username"
+            value={initialValues.username}
+            onChange={handleChange}
+          />
         </div>
         <div>
           <label>Password</label>
-          <TextField type="password" name="passowrd" />
+          <TextField
+            type="password"
+            name="password"
+            value={initialValues.password}
+            onChange={handleChange}
+          />
         </div>
         <div>
           <Button>Login</Button>
